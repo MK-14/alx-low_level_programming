860d02237057413687bc37a4cf600744d579efa0
MK-14 <sanamkara143@gmail.com>
2023-03-20 16:24:03 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#include <stdlib.h>
 
 /**
  * struct dog - a dog's basic info
@@ -16,8 +15,11 @@ struct dog
 	float age;
 	char *owner;
 };
-
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
@@ -25,4 +27,4 @@ void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
-#endif
+#endif/*DOG_H*/
